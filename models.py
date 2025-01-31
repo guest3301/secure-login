4ff4e528ee48346a6d5e2bbe9a4e90e08aa6482d
guest3301 <178640023+guest3301@users.noreply.github.com>
2025-01-31 10:33:54 +0000

    test

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    otp_secret = db.Column(db.String(16), nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
